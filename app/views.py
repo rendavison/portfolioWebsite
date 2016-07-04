5e281ca29ceafa920db892df6309ad2a66e6e9b2
Ren <Ren@Rens-MBP.wv.cc.cmu.edu>
2016-07-03 21:11:30 -0400

    added favicon and made a template for the single project pages

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from app import website
 @website.route('/')
 def index():
   return render_template('index.html')
+
+@website.route('/featured1.html')
+def featuredOne():
+  return render_template('featured1.html')
